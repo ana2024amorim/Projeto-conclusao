d87b28c85d8c001d2cdec85a1961ff3783a3d3ff
Leonardo <leonardo.amorim2011@gmail.com>
2024-10-06 11:06:38 -0300

    teste

--- teste.php
+++ teste.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>erwerwerwerwer</title>
+    <title>erwerwerwerwr</title>
 </head>
 <body>
     
