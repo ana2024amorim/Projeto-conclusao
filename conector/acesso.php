1af64d1b615701d4d7607b60bde20aae8f11a6a6
leonardo-2020 <leonardo.amorim2011@gmail.com>
2024-11-14 15:20:34 -0300

    versao-31

--- conector/acesso.php
+++ conector/acesso.php
@@ -1,7 +1,13 @@
 <?php
-// session_start();  // Inicia a sessão
+session_start(); // Inicia a sessão
+
 require_once "conector_db.php";
 
+// Verifica se os campos de matrícula e senha foram preenchidos
+if (empty($_POST['matricula']) || empty($_POST['password'])) {
+    header('Location: ../index.php?error=missing_fields');
+    exit();
+}
 
 $matricula = $_POST['matricula'];
 $password = $_POST['password']; 
@@ -11,7 +17,6 @@ $query = "SELECT * FROM tb_login WHERE matricula = ?";
 $stmt = $conn->prepare($query);
 
 if ($stmt === false) {
-    // Exibe o erro de SQL para debug
     die('Erro na preparação da consulta: ' . htmlspecialchars($conn->error));
 }
 
@@ -24,35 +29,36 @@ if ($user = $result->fetch_assoc()) {
     // Verifica se a senha corresponde usando hash no password_verify
     if (password_verify($password, $user['password'])) {
         $_SESSION['matricula'] = $matricula;
-        $_SESSION['permissao'] = $user['permissao']; // Armazena a permissão na sessão
-        
+        $_SESSION['permissao'] = $user['permissao'];
+        $_SESSION['foto'] = $user['foto']; // Armazena a foto do perfil na sessão
+
         // Redireciona baseado na permissão do usuário
         switch ($user['permissao']) {
-            case 'gerente':
-                header('Location: ../painel_admin.php');
+            case 'Gerente':
+                header('Location: ../modelo.php');
                 break;
-            case 'vendedor':
-                header('Location: ../pdv/pagina_inicial.php');
+            case 'Vendedor':
+                header('Location: ../pdv/vendedor.php');
                 break;
-            case 'estoquista':
+            case 'Estoquista':
                 header('Location: ../vendas/gestao_produtos.php');
                 break;
-            case 'caixa':
-                header('Location: ../paginacaixa.php');
+            case 'Caixa':
+                header('Location: ../PDV/caixa1.php');
+                break;
+            case 'Caixa':
+                header('Location: ../PDV/separado.php');
                 break;
             default:
-                // Se a permissão não corresponder a nenhuma das opções
                 header('Location: ../index.php?error=permission_denied');
                 break;
         }
         exit();
     } else {
-        // Senha incorreta
         header('Location: ../index.php?error=incorrect_password');
         exit();
     }
 } else {
-    // Usuário não encontrado
     header('Location: ../index.php?error=user_not_found');
     exit();
 }
